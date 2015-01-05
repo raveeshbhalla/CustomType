b726bdb35f660b1849f762a900978dd5a1a42f14
Raveesh Bhalla <raveeshbhalla90@gmail.com>
2015-01-05 23:43:56 +0530

    Remove ignored files

---
+++ library/src/main/java/customtype/Helper.java
@@ -0,0 +1,21 @@
+package customtype;
+
+import java.util.HashMap;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+public class Helper {
+
+	private static HashMap<String, Typeface> typefaces = new HashMap<String, Typeface>();
+	public static Typeface getTypeface(Context context, String path){
+		if (typefaces.containsKey(path)){
+			return typefaces.get(path);
+		}
+		else{
+			Typeface tf = Typeface.createFromAsset(context.getAssets(), path);
+			typefaces.put(path, tf);
+			return tf;
+		}
+	}
+}
---
+++ library/src/main/java/customtype/TextView.java
@@ -0,0 +1,28 @@
+package customtype;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+public class TextView extends android.widget.TextView {
+
+    public TextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		for (int i = 0; i < attrs.getAttributeCount(); i++) {
+			/**
+			 * Searching for the typeface attribute
+			 */
+			if (attrs.getAttributeName(i).equals("typeface")) {
+				String typeface = attrs.getAttributeValue(i);
+				if (!isInEditMode()) {
+					try {
+						setTypeface(Helper.getTypeface(context, typeface));
+					} catch (RuntimeException e) {
+                        e.printStackTrace();
+					}
+				}
+				break;
+			}
+		}
+	}
+}
